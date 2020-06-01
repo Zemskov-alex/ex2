41da7da10653fac8e9122dd67f7798c4bb427f2f
Alexey <zemskovale@mail.ru>
2020-06-01 20:16:24 +0300

    ex2

---
+++ ex2/src/ex2/ex2.java
@@ -0,0 +1,11 @@
+package ex2;
+
+public class ex2 {
+	int i = 0;{
+	System.out.println(++i);
+	System.out.print(i++);
+	System.out.print(i);
+
+
+}
+}
---
+++ ex3/src/ex3/ex3.java
@@ -0,0 +1,16 @@
+package ex3;
+
+public class ex3 {
+	public static void main (String[] args){
+		int a = 1;
+		int b = 2;
+		int c = 0;
+		System.out.println("a = " + a +"\nb = " + b +"\nc = " + c +"\nМеняем переменные местами!");
+		c = a;
+		a = b;
+		b = c;
+		System. out. println("a = " + a +"\nb = " + b +"\nc = " + c +"\nВот это ФОКУС!!!");
+
+}
+}
+
